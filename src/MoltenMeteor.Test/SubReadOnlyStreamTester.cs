42a3455d5191879857f639867b659569b90cdbfc
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2017-06-28 11:03:17 +0200

    Add sub stream reader and testing infrastructure

---
+++ src/MoltenMeteor.Test/SubReadOnlyStreamTester.cs
@@ -0,0 +1,112 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoltenMeteor.Test {
+
+    class SubReadOnlyStreamTester {
+
+        const string Alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        private Stream GenerateAlphabet() {
+            var bytes = Encoding.UTF8.GetBytes(Alphabet);
+            return new MemoryStream(bytes);
+        }
+
+        private SubReadOnlyStream GenerateReader(int offset, int length) {
+            var s = GenerateAlphabet();
+            return new SubReadOnlyStream(s, offset, length);
+        }
+
+        [Test]
+        public void SimpleRead() {
+            var reader = GenerateReader(0, Alphabet.Length);
+
+            Assert.AreEqual(Alphabet.Length, reader.Length);
+
+            var bytes = new byte[reader.Length];
+            reader.Read(bytes, 0, (int)reader.Length);
+            var reconstruct = Encoding.UTF8.GetString(bytes);
+            Assert.AreEqual(Alphabet, reconstruct);
+        }
+
+        [Test]
+        public void SliceReadBeginning() {
+            var s = GenerateAlphabet();
+            var reader = new SubReadOnlyStream(s, 0, 4);
+
+            Assert.AreEqual(reader.Length, 4);
+
+            var bytes = new byte[reader.Length];
+            reader.Read(bytes, 0, (int)reader.Length);
+            var reconstruct = Encoding.UTF8.GetString(bytes);
+            Assert.AreEqual(Alphabet.Substring(0, 4), reconstruct);
+        }
+
+        [Test]
+        public void SliceReadMiddle() {
+            var s = GenerateAlphabet();
+            var reader = new SubReadOnlyStream(s, 4, 6);
+
+            Assert.AreEqual(reader.Length, 6);
+
+            var bytes = new byte[reader.Length];
+            reader.Read(bytes, 0, (int)reader.Length);
+            var reconstruct = Encoding.UTF8.GetString(bytes);
+            Assert.AreEqual(Alphabet.Substring(4, 6), reconstruct);
+        }
+
+        [Test]
+        public void SliceReadEnd() {
+            var s = GenerateAlphabet();
+            var reader = new SubReadOnlyStream(s, 25, 1);
+
+            Assert.AreEqual(reader.Length, 1);
+
+            char ending = (char)reader.ReadByte();
+            Assert.AreEqual(ending, 'z');
+        }
+
+        [Test]
+        public void SliceReadOver() {
+            var s = GenerateAlphabet();
+            var reader = new SubReadOnlyStream(s, 25, 1);
+
+            Assert.AreEqual(reader.Length, 1);
+
+            var bytes = new byte[2];
+            int readBytes = reader.Read(bytes, 0, 2);
+            Assert.AreEqual(readBytes, 1);
+            Assert.AreEqual((char)bytes[0], 'z');
+            Assert.AreEqual(bytes[1], 0);
+        }
+
+        [Test]
+        public void SliceReadLimited() {
+            var s = GenerateAlphabet();
+            var reader = new SubReadOnlyStream(s, 2, 1);
+
+            Assert.AreEqual(reader.Length, 1);
+
+            var bytes = new byte[2];
+            int readBytes = reader.Read(bytes, 0, 2);
+            Assert.AreEqual(readBytes, 1);
+            Assert.AreEqual((char)bytes[0], 'c');
+            Assert.AreEqual(bytes[1], 0);
+        }
+
+        [Test]
+        public void SliceTooLong() {
+            var s = GenerateAlphabet();
+            Assert.Catch<ArgumentOutOfRangeException>(() => {
+                var reader = new SubReadOnlyStream(s, 25, 10);
+            });
+        }
+
+    }
+
+}
---
+++ src/MoltenMeteor/SubReadOnlyStream.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoltenMeteor {
+
+    /// <summary>
+    /// Read-only view on a stream's subsection.
+    /// </summary>
+    public class SubReadOnlyStream : Stream {
+
+        private readonly Stream _parent;
+        private readonly long _parentStart;
+        private readonly long _length;
+        private long _parentPosition;
+
+        public SubReadOnlyStream(Stream parent, long startOffset, long length) {
+            if (startOffset + length > parent.Length)
+                throw new ArgumentOutOfRangeException("Parent stream is not long enough");
+
+            _parent = parent;
+            _parentStart = startOffset;
+            _length = length;
+            _parentPosition = startOffset;
+        }
+
+        public override bool CanRead => _parent.CanRead;
+
+        public override bool CanSeek => true;
+
+        public override bool CanWrite => false;
+
+        public override long Length => _length;
+
+        public override long Position {
+            get => _parentPosition - _parentStart;
+            set {
+                if(value < 0 || value > (_length)) {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                _parentPosition = (_parentStart + value);
+            }
+        }
+
+        public override void Flush() {
+            // Nop
+        }
+
+        public override int Read(byte[] buffer, int offset, int count) {
+            int maxCount = (int)(_length - (_parentPosition - _parentStart));
+            if (count > maxCount)
+                count = maxCount;
+
+            _parent.Position = _parentPosition;
+            return _parent.Read(buffer, offset, count);
+        }
+
+        private long GetParentPosition(SeekOrigin origin) {
+            switch(origin) {
+                default:
+                    throw new ArgumentException("Unknown seek origin", nameof(origin));
+                case SeekOrigin.Begin:
+                    return _parentStart;
+                case SeekOrigin.Current:
+                    return _parentPosition;
+                case SeekOrigin.End:
+                    return _parentStart + _length;
+            }
+        }
+
+        public override long Seek(long offset, SeekOrigin origin) {
+            var destination = GetParentPosition(origin) + offset;
+            if (destination < _parentStart)
+                throw new IOException("Cannot seek to negative position");
+
+            _parentPosition = destination;
+            return _parentPosition;
+        }
+
+        public override void SetLength(long value) {
+            throw new InvalidOperationException("Cannot change length of read-only stream");
+        }
+
+        public override void Write(byte[] buffer, int offset, int count) {
+            throw new InvalidOperationException("Cannot write to read-only stream");
+        }
+    }
+
+}
