b60b78fb1848ac87e611f3464b075f0559b1cb30
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2017-08-01 23:46:06 +0200

    Add stream view reader to Reader interface

--- src/MoltenMeteor/Reader.cs
+++ src/MoltenMeteor/Reader.cs
@@ -37,7 +37,7 @@ namespace MoltenMeteor {
                 throw new ArgumentException($"Stream encoded using unsupported Molten Meteor v.{Version}");
             }
 
-            Identifier = new Guid(_reader.ReadBytes(16));            
+            Identifier = new Guid(_reader.ReadBytes(16));
         }
 
         public void Dispose() {
@@ -47,6 +47,9 @@ namespace MoltenMeteor {
         /// <summary>
         /// Reads a data field from an offset as a byte array.
         /// </summary>
+        /// <remarks>
+        /// Data is copied from the original stream.
+        /// </remarks>
         public (int id, byte[] data) ReadAsArray(long offset) {
             _reader.BaseStream.Position = offset;
 
@@ -57,6 +60,24 @@ namespace MoltenMeteor {
             var data = _reader.ReadBytes((int)length);
 
             return (id, data);
+        }
+
+        /// <summary>
+        /// Reads a data field from an offset as an in-memory stream.
+        /// </summary>
+        /// <remarks>
+        /// Data is not copied from the original stream. The returned data
+        /// stream is a view on the original stream and access is not synchronized.
+        /// </remarks>
+        public (int id, Stream data) ReadAsStream(long offset) {
+            _reader.BaseStream.Position = offset;
+
+            var id = _reader.ReadInt32();
+            var length = _reader.ReadUInt32();
+            if (length > int.MaxValue)
+                throw new ArgumentException("Blob field too large");
+
+            return (id, new SubReadOnlyStream(_reader.BaseStream, _reader.BaseStream.Position, length));
         }
 
     }
